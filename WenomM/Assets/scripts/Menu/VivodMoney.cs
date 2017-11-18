9027b9923185a30cc37bade22db01e5ff04c46f8
admin-ПК\admin <tduk-tik@mail.ru>
2017-11-19 00:23:06 +0300

    +

---
+++ WenomM/Assets/scripts/Menu/VivodMoney.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VivodMoney : MonoBehaviour {
+	public Text text;
+	public GameObject glob;
+
+	void Start () {
+		glob = GameObject.FindGameObjectWithTag ("glob");
+	}
+	
+
+	void Update () {
+		text.text =glob.GetComponent<global> ().money.ToString();
+	}
+}
