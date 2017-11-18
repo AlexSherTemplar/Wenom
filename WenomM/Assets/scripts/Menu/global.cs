64033ee928dcd3ac12dc7ad585d1c3f395334a26
admin-ПК\admin <tduk-tik@mail.ru>
2017-11-19 00:20:50 +0300

    Create RobotMenu

--- WenomM/Assets/scripts/Menu/global.cs
+++ WenomM/Assets/scripts/Menu/global.cs
@@ -17,4 +17,25 @@ public class global : MonoBehaviour {
 	void Update () {
 		
 	}
+	public void MaxHP(int ims){
+		maxhp =ims;
+	}
+	public void MaxEnergy(int ism){
+		maxEnergy = ism;
+	}
+	public void MaxTeplo(int ism){
+		maxteplo = ism;
+	}
+	public void fstWeap(int ism){
+		fstW = ism;
+	}
+	public void SecWeap(int ism){
+		secW = ism;
+	}
+	public void Osnova(int ism){
+		osn = ism;
+	}
+	public void Plata(int ism){
+		plata = ism;
+	}
 }
